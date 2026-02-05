2c19ef89bcfe59365793bd14968261784d1fecae
mlahmadmostafa <ml.ahmadmostafa@gmail.com>
2026-02-05 15:53:34 +0200

    feat: Implement user registration endpoint in auth router with password hashing.

--- project3 Todos CRUD SQLite/routers/auth.py
+++ project3 Todos CRUD SQLite/routers/auth.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
+
+
+
+router = APIRouter()
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class CreateUserRequest(BaseModel):
@@ -12,11 +18,17 @@ class CreateUserRequest(BaseModel):
     role:str
     # ID is auto created
     # Every user is active by default
-router = APIRouter()
-@router.get("/auth/")
+
+@router.post("/auth/")
 async def create_user(create_user_request: CreateUserRequest):
-    user_model = Users.query(
-        email = CreateUserRequest.
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+        role = create_user_request.role,
+        is_active = True
     )
     
-    return {'user':'authenticated'}
+    return create_user_model
